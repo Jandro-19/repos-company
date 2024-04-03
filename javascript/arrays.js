7bb89f15a9fb193b2cc5ce8c049c7f8c8baf2825
Alejandro <alejandro.agustench@hotmail.com>
2024-04-03 13:25:43 +0200

    13:25 03/04/2024

---
+++ javascript/arrays.js
@@ -0,0 +1,31 @@
+// // Array de abcde
+// let array = ['a', 'b', 'c', 'd',  'e']
+// array.forEach((element) => console.log(element))
+
+// // Array de 5 a 0
+
+// let numeros = 5;
+
+// while (numeros > -1) {
+//     console.log(número);
+//     numeros--;
+// }
+
+// Let iteraciones
+
+let numero = 1;
+let i = 0;
+do{
+    if(i === 0){
+        i++;
+        numero--;
+        console.log(numero);
+    } else{
+        numero++;
+        console.log(numero);
+    }
+}
+while (numero < 5);
+
+
+
--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,30 @@
-let academia = 'academia'
-let escuela = 'escuela'
-console.log(academia.startsWith('aca')) 
+ // Crear una variable llamada hola que diga 'Hola'
+let hola = "Hola "
+ // Crea una función (sayHello) que tenga la variable hola 
+ // Hacer un console.log con un repeat(3) para que repita la variable 3 veces 
+function sayHello (hola){
+    console.log(hola.repeat(3))
+}
+ // Llamar a la función
+sayHello(hola)
+//
+//
+//
+// I ❤️ Code con FOR
+for (let love = 0; love < 10; love++){
+    console.log('I ❤️ Code');
+}
+
+// I ❤️ Code con While
+
+let i = 0;
+let codelove = false;
+
+while (codelove === false) {
+    console.log('I ❤️ Code');
+    i++;
+    if (i === 10) {
+        codelove = true;
+    }
+}
+
